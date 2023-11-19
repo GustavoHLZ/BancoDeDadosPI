df8fd32a144873d1b0933ce5445cbd3255032040
Luigi <luigi.v01@aluno.ifsc.edu.br>
2023-11-19 18:14:11 -0300

    Select ALL Quartos

--- banco_Pi2.sql
+++ banco_Pi2.sql
@@ -428,6 +428,7 @@ Select * FROM Servicos;
 Select * FROM Servicos_Consumidos;
 Select * FROM Avaliacoes;
 Select * FROM Espacos;
+Select * FROM Quartos;
 
 update Info_Login set Login = 'Kaua', Senha = 12783734 where ID_usuario in (1);
 update Info_Login set Login = 'Zed', Senha = 724587453 where ID_usuario in (2);
